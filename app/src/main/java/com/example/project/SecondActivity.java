71e12dc99369ea9b2663a1476c9f3647571f59cf
c22abdha <c22abdha@student.his.se>
2023-05-04 16:00:38 +0200

    Write data to Shared Preferences using EditText

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,40 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+    private SharedPreferences sharedPreferences;
+    private final String key = String.valueOf(R.string.ShowData);
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        sharedPreferences = getSharedPreferences(key, MODE_PRIVATE);
+
+        button = findViewById(R.id.ViewButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                saveUserData();
+                finish();
+            }
+        });
+    }
+    void saveUserData() {
+        EditText input = findViewById(R.id.Writeinput);
+        String data = input.getText().toString();
+        SharedPreferences.Editor sharedPrefDataEditor = sharedPreferences.edit();
+        sharedPrefDataEditor.putString(key, data);
+        sharedPrefDataEditor.apply();
     }
 }
