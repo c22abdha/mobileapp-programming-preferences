d3adf3fe44be1e73175a0bac6ac45a2c62591ee7
c22abdha <c22abdha@student.his.se>
2023-05-04 14:53:15 +0200

    i created a SecondActivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView sharedresult;
+    private final String key = String.valueOf(R.string.viewData);
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
